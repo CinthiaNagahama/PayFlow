9be3ba526ab41ae3fc2ab6d850ea15d664fa1dcf
Cinthia M. N. Ungefehr <ciin.nagahama@gmail.com>
2021-06-25 18:16:24 -0300

    Created new bank slips config

---
+++ lib/modules/insert_slip/insert_slip_controller.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/slip_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class InsertSlipController {
+  final formKey = GlobalKey<FormState>();
+  SlipModel model = SlipModel();
+
+  String? validateName(String? value) => value?.isEmpty ?? true ? "O nome do boleto não pode ser vazio" : null;
+  String? validateDueDate(String? value) {
+    if (value?.isEmpty == true) {
+      return "A data de vencimento não pode ser vazia";
+    }
+
+    if (int.parse(value!.substring(0, 2)) > 31 ||
+        int.parse(value.substring(3, 5)) > 12 ||
+        int.parse(value.substring(6, 10)) < 2021) {
+      return "Data inválida";
+    }
+  }
+
+  String? validateValue(double? value) => value == 0 ? "Insira um valor maio que R\$ 0,00" : null;
+  String? validateBarcode(String? value) => value?.isEmpty ?? true ? "O código do boleto não pode ser vazio" : null;
+
+  void onChange({String? name, String? dueDate, double? value, String? barcode}) {
+    model = model.copyWith(name: name, dueDate: dueDate, value: value, barcode: barcode);
+  }
+
+  Future<void> saveSlip() async {
+    try {
+      final instance = await SharedPreferences.getInstance();
+      final slipList = instance.getStringList("slips") ?? <String>[];
+      slipList.add(model.toJson());
+      await instance.setStringList("slips", slipList);
+      return;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> registerSlip() async {
+    final form = formKey.currentState;
+
+    if (form!.validate()) {
+      return await saveSlip();
+    }
+  }
+}
--- lib/modules/insert_slip/insert_slip_screen.dart
+++ lib/modules/insert_slip/insert_slip_screen.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:payflow/modules/insert_slip/insert_slip_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/input_text/input_text_widget.dart';
 import 'package:payflow/shared/widgets/label_buttons/label_buttons_widget.dart';
 
-class InsertSlipScreen extends StatelessWidget {
-  const InsertSlipScreen({Key? key}) : super(key: key);
+class InsertSlipScreen extends StatefulWidget {
+  final String? barcode;
+  const InsertSlipScreen({Key? key, this.barcode}) : super(key: key);
+
+  @override
+  _InsertSlipScreenState createState() => _InsertSlipScreenState();
+}
+
+class _InsertSlipScreenState extends State<InsertSlipScreen> {
+  final controller = InsertSlipController();
+
+  final moneyInputTextController = MoneyMaskedTextController(
+    leftSymbol: "R\$",
+    decimalSeparator: ",",
+  );
+  final duaDateInputTextController = MaskedTextController(mask: "00/00/0000");
+  final barcodeInputTextController = TextEditingController();
+
+  @override
+  void initState() {
+    if (widget.barcode != null) {
+      barcodeInputTextController.text = widget.barcode!;
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,40 +44,64 @@ class InsertSlipScreen extends StatelessWidget {
           color: AppColors.input,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 40, right: 40, bottom: 24),
-              child: Text(
-                "Preencha os dados do boleto.",
-                style: AppTextStyles.titleBoldHeading,
-                textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 40, right: 40, bottom: 24),
+                child: Text(
+                  "Preencha os dados do boleto.",
+                  style: AppTextStyles.titleBoldHeading,
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
-            InputTextW(
-              label: "Nome do boleto",
-              icon: Icons.description_outlined,
-              onChanged: (value) {},
-            ),
-            InputTextW(
-              label: "Vencimento",
-              icon: FontAwesomeIcons.timesCircle,
-              onChanged: (value) {},
-            ),
-            InputTextW(
-              label: "Valor",
-              icon: Icons.account_balance_wallet_outlined,
-              onChanged: (value) {},
-            ),
-            InputTextW(
-              label: "Código do boleto",
-              icon: FontAwesomeIcons.barcode,
-              onChanged: (value) {},
-            ),
-          ],
+              Form(
+                key: controller.formKey,
+                child: Column(
+                  children: [
+                    InputTextW(
+                      label: "Nome do boleto",
+                      validator: controller.validateName,
+                      icon: Icons.description_outlined,
+                      onChanged: (value) {
+                        controller.onChange(name: value);
+                      },
+                    ),
+                    InputTextW(
+                      label: "Vencimento",
+                      controller: duaDateInputTextController,
+                      validator: controller.validateDueDate,
+                      icon: FontAwesomeIcons.timesCircle,
+                      onChanged: (value) {
+                        controller.onChange(dueDate: value);
+                      },
+                    ),
+                    InputTextW(
+                      label: "Valor",
+                      controller: moneyInputTextController,
+                      validator: (_) => controller.validateValue(moneyInputTextController.numberValue),
+                      icon: Icons.account_balance_wallet_outlined,
+                      onChanged: (value) {
+                        controller.onChange(value: moneyInputTextController.numberValue);
+                      },
+                    ),
+                    InputTextW(
+                      label: "Código do boleto",
+                      controller: barcodeInputTextController,
+                      validator: controller.validateBarcode,
+                      icon: FontAwesomeIcons.barcode,
+                      onChanged: (value) {
+                        controller.onChange(barcode: value);
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: LabelButtonsW(
@@ -61,7 +110,10 @@ class InsertSlipScreen extends StatelessWidget {
           Navigator.pop(context);
         },
         rightLabel: "Cadastrar",
-        rightOnPressed: () {},
+        rightOnPressed: () async {
+          await controller.registerSlip();
+          Navigator.pop(context);
+        },
         enablePrimaryColorRight: true,
       ),
     );
